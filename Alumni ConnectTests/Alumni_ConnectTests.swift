97d6bbeb90f8883ccd8575ae0cdf9e91c0a12875
Gianmaria Biselli <gianmariabiselli@gmail.com>
2018-06-08 11:44:31 -0400

    Initial Commit

---
+++ Alumni ConnectTests/Alumni_ConnectTests.swift
@@ -0,0 +1,36 @@
+//
+//  Alumni_ConnectTests.swift
+//  Alumni ConnectTests
+//
+//  Created by Gianmaria Biselli on 6/8/18.
+//  Copyright © 2018 Gianmaria Biselli. All rights reserved.
+//
+
+import XCTest
+@testable import Alumni_Connect
+
+class Alumni_ConnectTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
